be63923ee3a69c14326a56f166ee974f8ec9465d
Ivan Zabaleta <ivanzabalm@gmail.com>
2024-06-02 20:55:20 +0200

    last code modification

--- test/sb3_test_A2C.py
+++ test/sb3_test_A2C.py
@@ -3,21 +3,21 @@ import sys
 sys.path.append('../')
 
 from carla_env import CarlaEnv
-from stable_baselines3 import PPO
+from stable_baselines3 import A2C
 
 NUM_EPISODES = 5
 
-models_dir = "../models/PPO/"
-model_path = f"{models_dir}/PPO-Agent-1000K.zip"
+models_dir = "../models/A2C/"
+model_path = f"{models_dir}/A2C-Agent-500K.zip"
 
 # Start the enviroment 
 carla_env = CarlaEnv()
 carla_env.reset()
 
 # Load the specific model
-model = PPO.load(model_path, env=carla_env)
+model = A2C.load(model_path, env=carla_env)
 
-print("\nPPO AGENT")
+print("\nA2C AGENT")
 print("_________")
 for i in range(NUM_EPISODES):
     obs = carla_env.reset()
@@ -28,3 +28,4 @@ for i in range(NUM_EPISODES):
         obs, reward, done, info = carla_env.step(action)
 
     print(f"Episode {i}: {reward =}")
+
